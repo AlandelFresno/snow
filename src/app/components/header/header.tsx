db2728f2d82498d3c479f2b611ef98bb184589c3
Alan del Fresno <alandelfresno2@gmail.com>
2024-06-10 22:21:43 -0300

    Add Scroll to top

--- src/app/components/header/header.tsx
+++ src/app/components/header/header.tsx
@@ -1,21 +1,22 @@
 'use client';
-import React, { useRef } from 'react';
+import React from 'react';
 import styles from './header.module.scss';
 
 const header = () => {
-  const handleClick = (goTo: string) => {
-    const element = document.getElementById(goTo);
+  const handleClick = () => {
+    const element = document.getElementById('main');
     element?.scrollIntoView({ behavior: 'smooth' });
   };
 
   return (
     <div className={styles.container}>
-      <h1>SNOW</h1>
+      <h1 onClick={handleClick}>SNOW</h1>
       <div className={styles.links}>
-        <a href='#about'>Sobre nosotros</a>
-        <a href='#services'>Servicios</a>
-        <a href='#contact'>Contacto</a>
+        <a href="#services">Servicios</a>
+        <a href="#about">Sobre nosotros</a>
+        <a href="#contact">Contacto</a>
       </div>
+      <div className={styles.burgerMenu}></div>
     </div>
   );
 };
