5c45acf9ba218e75c13e533a9b5d3399725f7da5
Alan del Fresno <alandelfresno2@gmail.com>
2024-06-10 22:22:07 -0300

    Service Desktop improvement

--- src/app/components/services/services.tsx
+++ src/app/components/services/services.tsx
@@ -1,20 +1,21 @@
 import React from 'react';
 import styles from './services.module.scss';
+import Image from 'next/image';
 
 const services = () => {
   return (
     <div className={styles.container} id="services">
+      <h3>Servicios</h3>
       <div className={styles.service_container}>
-        <h3>Servicios</h3>
-        <div>
+        <div className={styles.list_container}>
           <p>
-            Mantenimiento, reparación e instalación de aires acondicionados.{' '}
+            Mantenimiento, reparación e instalación de aires acondicionados.
           </p>
           <p>Mantenimiento, reparación e instalación de grupos generadores.</p>
-          <p>Mantenimiento de centrales telefonicas publicas TDM e IP.</p>
+          <p>Mantenimiento de centrales telefónicas públicas TDM e IP.</p>
         </div>
       </div>
-      <img />
+      <img src="/assets/images/chip.jpg" />
     </div>
   );
 };
