974dd91f9b362fcde182e631fa0c267560ea32cd
Alan del Fresno <alandelfresno2@gmail.com>
2024-06-10 22:11:42 -0300

    Add SEO

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,12 @@ import About from './components/about/about';
 import Contact from './components/contact/contact';
 import Header from './components/header/header';
 import Services from './components/services/services';
+import SEO from './seo';
 
 export default function Home() {
   return (
-    <main className="">
+    <main id="main">
+      <SEO pageTitle="Snow" PageDescription="Servicios a tu disposición" />
       <Header />
       <Services />
       <About />
