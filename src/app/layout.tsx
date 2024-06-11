73d802763e3798d9864eb4eadf20168f69c79ec5
Alan del Fresno <alandelfresno2@gmail.com>
2024-06-10 22:11:57 -0300

    Fix font family not working

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Sofia+Sans:ital,wght@0,1..1000;1,1..1000&display=swap"
+          rel="stylesheet"
+        />
+      </head>
       <body>{children}</body>
     </html>
   );
