4f3774562c7eb1b61d03da38a5877b4619b1d1e1
Alan del Fresno <alandelfresno2@gmail.com>
2024-06-23 19:58:19 -0300

    Header complete

--- src/app/components/header/header.tsx
+++ src/app/components/header/header.tsx
@@ -1,22 +1,55 @@
 'use client';
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './header.module.scss';
 
 const header = () => {
+  const [showLinks, setShowLinks] = useState(true);
+
   const handleClick = () => {
     const element = document.getElementById('main');
     element?.scrollIntoView({ behavior: 'smooth' });
   };
 
+  const burgerClick = () => {
+    setShowLinks((v) => !v);
+  };
+
   return (
     <div className={styles.container}>
-      <h1 onClick={handleClick}>SNOW</h1>
+      <img
+        src="/assets/images/snow/snow_isologo_sin_rubro_version_positivo_svg.svg"
+        className={styles.isologo}
+        onClick={handleClick}
+      />
+      <div className={styles.burgerMenu}>
+        {showLinks ? (
+          <img
+            onClick={burgerClick}
+            src="/assets/images/close.svg"
+            className={styles.burger}
+          />
+        ) : (
+          <img
+            onClick={burgerClick}
+            src="/assets/images/burger.svg"
+            className={styles.burger}
+          />
+        )}
+      </div>
       <div className={styles.links}>
         <a href="#services">Servicios</a>
         <a href="#about">Sobre nosotros</a>
         <a href="#contact">Contacto</a>
       </div>
-      <div className={styles.burgerMenu}></div>
+      <div
+        className={
+          `${styles.showLinks}` + ' ' + `${showLinks ? styles.show : ''}`
+        }
+      >
+        <a href="#services">Servicios</a>
+        <a href="#about">Sobre nosotros</a>
+        <a href="#contact">Contacto</a>
+      </div>
     </div>
   );
 };
